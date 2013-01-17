ab640844bb16f4f9d93f9556dc50b6a6ad60edc6
ali <ali@ali-R428-P428-R478.(none)>
2013-01-17 10:59:48 +0400

    Step two

--- wd_of_ants.c
+++ wd_of_ants.c
@@ -4,8 +4,15 @@
 
 
 
+
+static wd_of_ants my_world;
+
+ 
+
 int wd_of_ants_init( void )
 {
+	/* ----- my_word полностью создадим как надо ----- */
+
 	printf( "I am happy say you that: Make the Word of Ants complete!\n" );
 	return 0;
 }
