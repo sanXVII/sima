188ee43f35be0944c74d50ce2ef73819180a7b15
Alexander <sanXVII@gmail.com>
2013-03-17 14:35:21 +0400

    Update rtree.h

--- rtree.h
+++ rtree.h
@@ -2,12 +2,22 @@
 #define RTREE_H_
 
 
+typedef struct rtree_n
+{
+} rtree_n;
 
+
+#define RTREE_NBLOCK_SZ (1000)
+typedef struct rtree_nblk
+{
+  rtree_n node[ RTREE_NBLOCK_SZ ];
+  struct rtree_nblk * next;
+} rtree_nblk;
 
 
 typedef struct rtree
 {
-  
+  rtree_nblk first_blk;
 } rtree;
 
 
