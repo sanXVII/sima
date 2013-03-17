730167a353a4c84bdc4d3360cfda02fe625d0c53
ali <ali@ali-R428-P428-R478.(none)>
2013-03-17 22:41:58 +0400

    ++

---
+++ rtree.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+
+#include "rtree.h"
+
+
+rtree * new_rtree( void )
+{
+	return 0l
+}
+
+void del_rtree( rtree * rt )
+{
+}
+
+
+void to_rtree( rtree * rt, float x, float y, void * p )
+{
+}
+
--- rtree.h
+++ rtree.h
@@ -10,8 +10,8 @@ typedef struct rtree_n
 #define RTREE_NBLOCK_SZ (1000)
 typedef struct rtree_nblk
 {
-  rtree_n node[ RTREE_NBLOCK_SZ ];
-  struct rtree_nblk * next;
+	rtree_n node[ RTREE_NBLOCK_SZ ];
+	struct rtree_nblk * next;
 } rtree_nblk;
 
 
@@ -19,15 +19,22 @@ typedef struct rtree
 {
   
   
-  rtree_nblk first_blk;
-  rtree_nblk * cur_blk;
-  int cur_cnt;
+	rtree_nblk first_blk;
+	rtree_nblk * cur_blk;
+	int cur_cnt;
   
 } rtree;
 
 
 rtree * new_rtree( void );
 void del_rtree( rtree * rt );
+
+
+/* Add point to R-tree
+ * rt   - main data
+ * x, y - point coordinates
+ * p    - pointer to customer data */
+void to_rtree( rtree * rt, float x, float y, void * p );
 
 
 #endif /* RTREE_H_ */
